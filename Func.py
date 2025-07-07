0902b0855d7b4dfc734656c313a945ce2ea99579
XenSec_AI <muhammadusman.consulting@gmail.com>
2025-07-07 22:02:48 +0200

    Create Func.py
    Function with no parameter.

---
+++ Func.py
@@ -0,0 +1,10 @@
+def test():
+    print("This is a test function from Func.py")
+    print("It is used to demonstrate the import functionality in Python.")
+    print("You can call this function from another file to see how it works.")
+
+test()
+print('Once Again Please!')
+test()
+print('This is the end of the Func.py file. You can now import this file in another Python script and use the test function.')
+print('Thank you for using this example!')
